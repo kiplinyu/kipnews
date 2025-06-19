c388eb1b12352cacf430a7be68d714e4ce1f03f1
“irpanXstrome“ <irpancocsuperdragon@gmail.com>
2025-06-19 17:07:53 +0700

    feat: add NewsEntity model for representing news articles

---
+++ lib/app/features/news/business/entities/news_entity.dart
@@ -0,0 +1,13 @@
+class NewsEntity {
+  final String id;
+  final String title;
+  final String summary;
+  final String imageUrl;
+
+  NewsEntity({
+    required this.id,
+    required this.title,
+    required this.summary,
+    required this.imageUrl,
+  });
+}
