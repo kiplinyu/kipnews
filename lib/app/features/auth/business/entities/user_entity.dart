9de43be39790e7657d938fb8c19bef4426d82e68
“irpanXstrome“ <irpancocsuperdragon@gmail.com>
2025-06-19 17:08:50 +0700

    feat: add UserEntity for representing user data and authentication

---
+++ lib/app/features/auth/business/entities/user_entity.dart
@@ -0,0 +1,13 @@
+class UserEntity {
+  final String id;
+  final String email;
+  final String name;
+  final String token;
+
+  UserEntity({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.token,
+  });
+}
