10786dd251995136372091f2ae19a40fb4629d86
“irpanXstrome“ <irpancocsuperdragon@gmail.com>
2025-06-20 14:19:52 +0700

    feat: refactor HomePage to use NewsEntity and integrate news provider for data fetching

--- lib/app/features/news/business/entities/news_entity.dart
+++ lib/app/features/news/business/entities/news_entity.dart
@@ -1,13 +1,32 @@
 class NewsEntity {
   final String id;
   final String title;
+  final String slug;
   final String summary;
+  final String content;
+  final String featuredImageUrl;
+  final String authorId;
+  final String category;
+  final List<String> tags;
+  final bool isPublished;
+  final DateTime publishedAt;
+  final int viewCount;
   final String imageUrl;
+
 
   NewsEntity({
     required this.id,
     required this.title,
+    required this.slug,
     required this.summary,
+    required this.content,
+    required this.featuredImageUrl,
+    required this.authorId,
+    required this.category,
+    required this.tags,
+    required this.isPublished,
+    required this.publishedAt,
+    required this.viewCount,
     required this.imageUrl,
   });
 }
--- lib/app/features/news/presentation/providers/news_provider.dart
+++ lib/app/features/news/presentation/providers/news_provider.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kipnews/app/features/news/business/repositories/news_repository.dart';
+import 'package:kipnews/app/features/news/data/datasources/news_remote_data_source.dart';
+import 'package:kipnews/app/features/news/data/repositories/news_repository_impl.dart';
 
+import '../../../../core/connection/dio_client.dart';
 import '../../business/entities/news_entity.dart';
 import '../../business/usecases/get_news.dart';
+
+final newsProvider = ChangeNotifierProvider((ref) {
+  final dio = ref.read(newsApi); // inject Dio
+  final remote = NewsRemoteDatasource(dio); // inject ke remote
+  final repo = NewsRepositoryImpl(remote);       // inject ke repository
+  final usecase = GetNews(repo); // inject ke usecase
+  return NewsProvider(usecase);                  // inject ke provider
+});
 
 class NewsProvider extends ChangeNotifier {
   final GetNews getNews;
