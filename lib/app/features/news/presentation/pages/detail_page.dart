c25dcd987a75caadc9945d9786a4226f31600ebd
“irpanXstrome“ <irpancocsuperdragon@gmail.com>
2025-06-20 15:18:09 +0700

    feat: update DetailPage to use NewsEntity for improved type safety and data access

--- lib/app/features/news/presentation/pages/detail_page.dart
+++ lib/app/features/news/presentation/pages/detail_page.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:kipnews/app/features/news/business/entities/news_entity.dart';
 
 // Halaman detail (placeholder)
 class DetailPage extends StatelessWidget {
-  final Map<String, dynamic> newsItem;
+  final NewsEntity newsItem;
 
   const DetailPage({super.key, required this.newsItem});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(newsItem['title'])),
+      appBar: AppBar(title: Text(newsItem.title)),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Text(newsItem['author']),
+        child: Text(newsItem.authorId),
       ),
     );
   }
